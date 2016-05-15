2b12b3a75a2c6f4bbda9ad8c536227674774a583
hary mindiar <mindiar@yahoo.com>
2016-05-16 00:09:39 +0700

    add pagination template

--- app/Components/Table/Table.jsx
+++ app/Components/Table/Table.jsx
@@ -1,24 +1,30 @@
 import React, { Component, PropTypes } from 'react';
 import TableHead from './TableHead';
 import TableRow from './TableRow';
+import Pagination from './Pagination';
 
 class Table extends Component {
     static propTypes = {
         heads: React.PropTypes.array,
         rows: React.PropTypes.array,
+        pagination: React.PropTypes.object
     }
 
     static defaultProps = {
         heads: [],
-        rows: []
+        rows: [],
+        pagination: {}
     }
 
     render() {
         return (
-            <table className="table table-bordered">
-                <TableHead heads={this.props.heads}/>
-                <TableRow rows={this.props.rows}/>
-            </table>
+            <div>
+                <table className="table table-bordered">
+                    <TableHead heads={this.props.heads}/>
+                    <TableRow rows={this.props.rows}/>
+                </table>
+                <Pagination options={this.props.pagination}/>
+            </div>
         );
     }
 }
--- app/index.js
+++ app/index.js
@@ -2,38 +2,50 @@ import React from 'react'
 import { render } from 'react-dom';
 import Table from './Components/Table/Table';
 
-var heads = [
-	{
-		'id': 1,
-		'name': 'No.'
-	},
-	{
-		'id': 2,
-		'name': 'Name',
-	},
-	{
-		'id': 3,
-		'name': 'Address'
-	},
+let heads = [
+    {
+        'id': 1,
+        'name': 'No.'
+    },
+    {
+        'id': 2,
+        'name': 'Name',
+    },
+    {
+        'id': 3,
+        'name': 'Address'
+    },
 ];
 
-var rows = [
-	{
-		'id': 10,
-		'values': {
-			id: 10,
-			name: 'John',
-			address: 'Jakarta'
-		}
-	},
-	{
-		'id': 11,
-		'values': {
-			id: 11,
-			name: 'John',
-			address: 'Jakarta'
-		}
-	}
+let rows = [
+    {
+        'id': 10,
+        'values': {
+            id: 10,
+            name: 'John',
+            address: 'Jakarta'
+        }
+    },
+    {
+        'id': 11,
+        'values': {
+            id: 11,
+            name: 'John',
+            address: 'Jakarta'
+        }
+    }
 ];
 
-render(<Table heads={heads} rows={rows}/>, document.getElementById('app'));
+let pagination = {
+    count: 100, // total rows
+    activeClass: 'active',
+    inactiveClass: '',
+    defaults: {
+        size: 10,        // Result per page
+        listLength: 2,  // Length pagination Result.
+        // eg:  listLength = 2; ==>  <<  5, 6, 7, 8, 9  >>
+        //      listLength = 1; ==>  <<  6, 7, 8  >>
+    }
+};
+
+render(<Table heads={heads} rows={rows} pagination={pagination}/>, document.getElementById('app'));
